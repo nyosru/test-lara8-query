7b1622c2bcf23bd6dc82f1f64915d7c9b70465b6
Сергей <1@php-cat.com>
2022-09-19 19:36:28 +0500

    add_db

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('ss', function () {
+
+    // добавление задания в очередь
+
+    dd(['добавить задание']);
+    
+    return view('welcome');
+
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
